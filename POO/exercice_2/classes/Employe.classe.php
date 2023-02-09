6bbc2407334f6f8ec204be34cecc34e3c15cbc7f
DioufFLR <geoffreyfleur@gmail.com>
2023-02-09 11:13:39 +0100

    exercice 2.1 POO

--- POO/exercice_2/classes/Employe.classe.php
+++ POO/exercice_2/classes/Employe.classe.php
@@ -8,101 +8,42 @@ class Employe
     private $_salaire;
     private $_service;
 
-    /**
-     * @return mixed
-     */
-    public function getNom()
-    {
-        return $this->_nom;
-    }
+//    Listes des setters
 
-    /**
-     * @param mixed $nom
-     */
+    public function getNom() { return $this->_nom; }
+    public function getPrenom() { return $this->_prenom; }
+    public function getDateEmb() { return $this->_dateEmb; }
+    public function getPoste() { return $this->_poste; }
+    public function getSalaire() { return $this->_salaire; }
+    public function getService() { return $this->_service; }
+
+//    Liste des getters
+
     public function setNom($nom): void
     {
         $this->_nom = $nom;
     }
-
-    /**
-     * @return mixed
-     */
-    public function getPrenom()
-    {
-        return $this->_prenom;
-    }
-
-    /**
-     * @param mixed $prenom
-     */
     public function setPrenom($prenom): void
     {
         $this->_prenom = $prenom;
     }
-
-    /**
-     * @return mixed
-     */
-    public function getDateEmb()
-    {
-        return $this->_dateEmb;
-    }
-
-    /**
-     * @param mixed $dateEmb
-     */
     public function setDateEmb($dateEmb): void
     {
         $this->_dateEmb = $dateEmb;
     }
-
-    /**
-     * @return mixed
-     */
-    public function getPoste()
-    {
-        return $this->_poste;
-    }
-
-    /**
-     * @param mixed $poste
-     */
     public function setPoste($poste): void
     {
         $this->_poste = $poste;
     }
-
-    /**
-     * @return mixed
-     */
-    public function getSalaire()
-    {
-        return $this->_salaire;
-    }
-
-    /**
-     * @param mixed $salaire
-     */
     public function setSalaire($salaire): void
     {
         $this->_salaire = $salaire;
     }
-
-    /**
-     * @return mixed
-     */
-    public function getService()
-    {
-        return $this->_service;
-    }
-
-    /**
-     * @param mixed $service
-     */
     public function setService($service): void
     {
         $this->_service = $service;
     }
+
 
 
 }
