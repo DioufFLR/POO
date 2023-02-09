9ad5ee0c6e376edaa2e2dc7b21bb026461c00610
DioufFLR <geoffreyfleur@gmail.com>
2023-02-09 11:27:56 +0100

    exercice 2 ajout d'un employé

--- POO/exercice_2/classes/Employe.php
+++ POO/exercice_2/classes/Employe.php
@@ -1,8 +1,8 @@
 <?php
 include ("Employe.classe.php");
 $e = new Employe();
-$e->setNom("Fleur");
-$e->setPrenom("Vivian");
+$e->setNom("Karot");
+$e->setPrenom("Julien");
 $e->setDateEmb("27.04.2008");
 $e->setPoste("Laveur de vaisselles");
 $e->setSalaire("20");
