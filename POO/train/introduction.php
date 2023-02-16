60f01620a6761fff2d8cb66dcdee9914a90a16ae
DioufFLR <geoffreyfleur@gmail.com>
2023-02-16 10:48:47 +0100

    train .2

--- POO/train/introduction.php
+++ POO/train/introduction.php
@@ -1,16 +1,30 @@
 <?php
 
+// Classe
 class Employe {
     public $nom;
     public $prenom;
-    public $age;
+    private $age;
 
     public function __construct($prenom, $nom, $age)
     {
         $this->prenom = $prenom;
         $this->nom = $nom;
-        $this->age = $age;
-        var_dump("Je suis construit");
+        $this->setAge($age);
+
+    }
+
+    public function setAge($age): void
+    {
+        if (is_int($age) && $age >= 1 && $age <= 120) {
+            $this->age = $age;
+        } else {
+            throw new Exception("L'âge d'un employé devrait être un entier entre 1 et 120!");
+        }
+    }
+    public function getAge()
+    {
+        return $this->age;
     }
 
     public function presentation()
@@ -19,15 +33,12 @@ class Employe {
     }
 }
 
-$employe1 = new Employe();
-$employe1->prenom = "Lior";
-$employe1->nom = "Chamla";
-$employe1->age = 32;
 
-$employe2 = new Employe();
-$employe2->prenom = "Magalie";
-$employe2->nom = "Pernin";
-$employe2->age = 32;
+
+$employe1 = new Employe("Kaido", "Owls", 23);
+$employe2 = new Employe("Roger", "Karat", 54);
+
+$employe2->setAge(42);
 
 $employe1->presentation();
 $employe2->presentation();
