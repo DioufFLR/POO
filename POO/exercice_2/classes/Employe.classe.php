a53edf2f1a74997a12e7eaab9f58f4c03b0b4bda
DioufFLR <geoffreyfleur@gmail.com>
2023-02-23 10:37:36 +0100

    2.2

--- POO/exercice_2/classes/Employe.classe.php
+++ POO/exercice_2/classes/Employe.classe.php
@@ -1,62 +1,46 @@
 <?php
 class Employe
 {
-    private $_nom;
-    private $_prenom;
-    private $_dateEmb;
-    private $_poste;
-    private $_salaire;
-    private $_service;
+    private $nom;
+    private $prenom;
+    private $dateEmb;
+    private $poste;
+    private $salaire;
+    private $service;
+
+//    construct
+    public function __construct($nom, $prenom, $dateEmb, $poste, $salaire, $service) {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->dateEmb = $dateEmb;
+        $this->poste = $poste;
+        $this->salaire = $salaire;
+        $this->service = $service;
+    }
 
 //    Listes des setters
 
-    public function getNom() { return $this->_nom; }
-    public function getPrenom() { return $this->_prenom; }
-    public function getDateEmb() { return $this->_dateEmb; }
-    public function getPoste() { return $this->_poste; }
-    public function getSalaire() { return $this->_salaire; }
-    public function getService() { return $this->_service; }
-
-//    Liste des getters
-
-    public function setNom($nom): void
-    {
-        $this->_nom = $nom;
-    }
-    public function setPrenom($prenom): void
-    {
-        $this->_prenom = $prenom;
-    }
-    public function setDateEmb($dateEmb): void
-    {
-        $this->_dateEmb = $dateEmb;
-    }
-    public function setPoste($poste): void
-    {
-        $this->_poste = $poste;
-    }
-    public function setSalaire($salaire): void
-    {
-        $this->_salaire = $salaire;
-    }
-    public function setService($service): void
-    {
-        $this->_service = $service;
-    }
+    public function getNom() { return $this->nom; }
+    public function getPrenom() { return $this->prenom; }
+    public function getDateEmb() { return $this->dateEmb; }
+    public function getPoste() { return $this->poste; }
+    public function getSalaire() { return $this->salaire; }
+    public function getService() { return $this->service; }
 
     public function __toString(): string
     {
-        return "Nom de l'employé : " . $this->_nom . " " . $this->_prenom . ".<br>" . "Depuis le : " . $this->_dateEmb . " au poste de " . $this->_poste . " avec un salaire de " . $this->_salaire . "K$ brut annuel." . "<br>" . "Il est dans le service " . $this->_service;
+        return "Nom de l'employé : " . $this->nom . " " . $this->prenom . ".<br>" . "Depuis le : " . $this->dateEmb . " au poste de " . $this->poste . " avec un salaire de " . $this->salaire . "K$ brut annuel." . "<br>" . "Il est dans le service " . $this->service;
     }
 
-    public function anciennete($_prenom, $_nom, $_dateEmb) {
-        $firstDate = new DateTime($_dateEmb);
+    /**
+     * On calcul l'ancienneté
+     */
+    public function anciennete($prenom, $nom): void
+    {
+        $firstDate = new DateTime($this->dateEmb);
         $secondDate = new \DateTime();
         $intvl = $firstDate->diff($secondDate);
 
-        echo $_prenom . " " . $_nom . " est dans l'entreprise depuis " . $intvl->y . " années " . $intvl->m . " mois et " . $intvl->d . " jours";
+        echo $prenom . " " . $nom . " est dans l'entreprise depuis " . $intvl->y . " années " . $intvl->m . " mois et " . $intvl->d . " jours";
     }
-
-//    TODO faire methode pour calculer timestamp
-
 }
--- POO/exercice_2/classes/Employe.php
+++ POO/exercice_2/classes/Employe.php
@@ -1,14 +1,12 @@
 <?php
 include ("Employe.classe.php");
-$e = new Employe();
-$e->setNom("Karot");
-$e->setPrenom("Julien");
-$e->setDateEmb("27.04.2008");
-$e->setPoste("Laveur de vaisselles");
-$e->setSalaire("20");
-$e->setService("Commercial");
+$e = new Employe("Karot", "Julien", "23.10.2019", "Surfeur", 13, "Commercial");
+
+$e2 = new Employe("Diallo", "Seydina", "25.03.2015", "Plongeur", 15, "Nettoyage");
 
 echo ($e);
+echo ($e2);
 echo "<br>";
-$e->anciennete("Julien", "Karot", "27.04.2008");
+
+$e->anciennete("Julien", "Karot");
 
