f70c8f57e0d07c0de6b8b4f25c4056541ebc75ec
DioufFLR <geoffreyfleur@gmail.com>
2023-02-24 09:47:33 +0100

    2.4

--- POO/exercice_2/classes/index.php
+++ POO/exercice_2/classes/index.php
@@ -7,18 +7,14 @@ $e2 = new Employe("Diallo", "Seydina", "25.03.2015", "Plongeur", 15, "Nettoyage"
 $e3 = new Employe("Malek", "Sandrine", "25.07.2017", "Comptable", 35, "Comptabilité");
 $e4 = new Employe("Teurkie", "Zakaria", "13.08.2000", "Soudeur", 25, "Entretient");
 $e5 = new Employe("Ben Mansour", "Amine", "08.09.2020", "Animateur", 16, "Animation");
+$e6 = new Employe("Mekalfia", "Farid", "08.09.2023", "Boss", 60, "Direction");
 
 echo ($e);
 echo ($e2);
 echo ($e3);
 echo ($e4);
 echo ($e5);
-
-$e->anciennete();
-$e2->anciennete();
-$e3->anciennete();
-$e4->anciennete();
-$e5->anciennete();
+echo ($e6);
 
 echo "<br>";
 
@@ -27,6 +23,7 @@ $e2->prime();
 $e3->prime();
 $e4->prime();
 $e5->prime();
+$e6->prime();
 
 $mag = new Magasin("Julien", "Karot", "Casto", "11 rue de la courge", "95300", "Sarcelles");
 
