74dab29952362c8ecb3f5f1407d0a4c4a4137b4f
DioufFLR <geoffreyfleur@gmail.com>
2023-02-27 10:36:35 +0100

    2.5

--- POO/exercice_2/classes/Employe.php
+++ POO/exercice_2/classes/Employe.php
@@ -17,26 +17,6 @@ class Employe extends Human
      */
     public function __toString(): string
     {
-        return "<br>" . "Nom de l'employé : " . $this->nom . " " . $this->prenom . ".<br>" . "Depuis le : " . $this->dateEmb . " au poste de " . $this->poste . " avec un salaire de " . $this->salaire . "K$ brut annuel." . "<br>" . "Il est dans le service " . $this->service . "<br>";
+        return "<br>" . "Nom de l'employé : " . $this->nom . " " . $this->prenom . ".<br>" . "Depuis le : " . $this->dateEmb . " au poste de " . $this->poste . " avec un salaire de " . $this->salaire . "K$ brut annuel." . "<br>" . "Il est dans le service " . $this->service;
     }
-
-    public function prime(): void
-    {
-        $firstDate = new DateTime($this->dateEmb);
-        $secondDate = new \DateTime();
-        $intvl = $firstDate->diff($secondDate);
-
-        $prime = (0.05 * $this->salaire) * 1000;
-        $primeAnciennete = $intvl->y * (0.02 * $prime);
-        $primeReel = ($primeAnciennete + $prime) * $intvl->y;
-
-        if ($intvl->y <= 1) {
-            echo "<br>" . $this->nom . " " . $this->prenom . " est dans l'entreprise depuis " .  $intvl->m . " mois et " . $intvl->d . " jours" . "<br>";
-        } else {
-            echo "<br>" . $this->nom . " " . $this->prenom . " est dans l'entreprise depuis " . $intvl->y . " années " . $intvl->m . " mois et " . $intvl->d . " jours" . "<br>";
-        }
-
-        echo $this->nom . " " . $this->prenom . " percevra une prime de " . $primeReel . " euros" . " au 30/11." . "<br>";
-    }
-
 }
--- POO/exercice_2/classes/index.php
+++ POO/exercice_2/classes/index.php
@@ -2,26 +2,34 @@
 require_once('Employe.php');
 require_once ('Magasin.php');
 
+// Employés
 $e = new Employe("Karot", "Julien", "23.10.2019", "Surfeur", 13, "Commercial", "Magasin 1");
 $e2 = new Employe("Diallo", "Seydina", "25.03.2015", "Plongeur", 15, "Nettoyage", "Magasin 2");
 $e3 = new Employe("Malek", "Sandrine", "25.07.2017", "Comptable", 35, "Comptabilité", "Magasin 3");
 $e4 = new Employe("Teurkie", "Zakaria", "13.08.2000", "Soudeur", 25, "Entretient", "Magasin 4");
 $e5 = new Employe("Ben Mansour", "Amine", "08.09.2020", "Animateur", 16, "Animation", "Magasin 5");
-$e6 = new Employe("Mekalfia", "Farid", "08.09.2023", "Boss", 60, "Direction", "Magasin 6");
+
+// Magasins
+$mag = new Magasin("Karot", "Julien", "23.10.2019", "Surfeur", 13, "Commercial", "Magasin 1", "23 rue du fou", 86000, "Poitiers" );
+$mag2 = new Magasin("Diallo", "Seydina", "25.03.2015", "Plongeur", 15, "Nettoyage", "Magasin 2", "1 rue de la verdure", 67000, "Strasbourg");
+$mag3 = new Magasin("Malek", "Sandrine", "25.07.2017", "Comptable", 35, "Comptabilité", "Magasin 3", "1 rue de la verdure", 44000, "Nantes");
+$mag4 = new Magasin("Teurkie", "Zakaria", "13.08.2000", "Soudeur", 25, "Entretient", "Magasin 4", "1 rue de la verdure", 75010, "Paris");
+$mag5 = new Magasin("Ben Mansour", "Amine", "08.09.2020", "Animateur", 16, "Animation", "Magasin 5", "1 rue de la verdure", 60000, "Beauvais");
 
 echo ($e);
+$mag->prime();
 echo ($e2);
+$mag2->prime();
 echo ($e3);
+$mag3->prime();
 echo ($e4);
+$mag4->prime();
 echo ($e5);
-echo ($e6);
+$mag5->prime();
 
-echo "<br>";
 
-$e->prime();
-$e2->prime();
-$e3->prime();
-$e4->prime();
-$e5->prime();
-$e6->prime();
+
+
+
+
 
